83a08a72e71cb9a8de939554bbb59a236b20bea0
Theodor <theodju@gmail.com>
2025-08-11 16:02:30 +0200

    1

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gdamore/tcell/v2"
+)
+
+func main() {
+
+	s, err := tcell.NewScreen()
+	if err != nil {
+		log.Fatalf("%+v", err)
+	}
+	if err := s.Init(); err != nil {
+		log.Fatalf("%+v", err)
+	}
+
+	defStyle := tcell.StyleDefault.Background(tcell.ColorReset).Foreground(tcell.ColorReset)
+	s.SetStyle(defStyle)
+
+	s.Clear()
+
+	quit := func() {
+		maybePanic := recover()
+		s.Fini()
+		if maybePanic != nil {
+			panic(maybePanic)
+		}
+	}
+	defer quit()
+
+
+	const gl = '▄' 
+
+	s.SetContent(0, 3, gl, nil, tcell.StyleDefault.Foreground(tcell.ColorRed).Background(tcell.ColorBlue))
+	
+
+	for {
+		s.Show()
+
+		ev := s.PollEvent()
+
+		switch ev := ev.(type) {
+		case *tcell.EventResize:
+			s.Sync()
+		case *tcell.EventKey:
+			if ev.Key() == tcell.KeyEscape || ev.Key() == tcell.KeyCtrlC {
+				return
+			}
+		}
+	}
+}
